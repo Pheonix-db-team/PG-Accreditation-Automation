06bedf057cd09e5eea2378017ea5c839b1e070a9
Yugam <yugam11parashar@gmail.com>
2023-03-06 14:12:53 +0530

    Deep dive props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 function App() {
   const num = 46;
+  const str = "xyz";
   return (
     <div className="App">
       //passing var w prop
-      <HelloWorld num={num} />
+      <HelloWorld propnum={num} propstr={str} />
     </div>
   );
 }
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-function HelloWorld(props) {
+//multi prop
+function HelloWorld({ propnum, propstr }) {
     //add props argument
     const greeting = "Hello World!!";
     const followup_greeting = "I am getting started with react";
@@ -13,7 +13,8 @@ function HelloWorld(props) {
         <div>
             <div>{greeting}</div>
             <div>{followup_greeting}</div>
-            <div> Props passed num is {props.num}</div>
+            <div> Props passed num is {propnum}</div>
+            <div> Props passed str is {propstr}</div>
             <div> Computing {num1} + {num2} to {num1 + num2}</div>
             //internal link react router dom
             <a href={link}> Go to google</a>
