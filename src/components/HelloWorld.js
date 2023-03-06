d84ac050d4926c632454b9e7457b7e49f4ef48a9
Yugam <yugam11parashar@gmail.com>
2023-03-06 14:23:15 +0530

    :lipstick: Inline CSS wrapup

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -6,11 +6,13 @@ function HelloWorld({ propnum, propstr }) {
     const followup_greeting = "I am getting started with react";
     const num1 = 12;
     const num2 = 13;
-    const link = "http://www.google.com"
+    const link = "http://www.google.com";
+    const myStyle = { backgroundColor: 'cornsilk', padding: '10px', fontSize: '20px' };
     return (
         //in curly braces look for var else string
         //React functional component can only return only one ele
-        <div style={{ backgroundColor: 'red' }}>
+
+        <div style={myStyle}>
             <div>{greeting}</div>
             <div>{followup_greeting}</div>
             <div> Props passed num is {propnum}</div>
