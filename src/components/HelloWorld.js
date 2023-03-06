ca2897cd3b5c295e7f53dcdcc160d90c4db4514d
Yugam <yugam11parashar@gmail.com>
2023-03-06 14:03:50 +0530

    :rocket:Finished first custom Component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function HelloWorld() {
+    const greeting = "Hello World!!";
+    const followup_greeting = "I am getting started with react";
+    const num1 = 12;
+    const num2 = 13;
+    const link = "http://www.google.com"
     return (
-        <div>Hello World</div>
+        //in curly braces look for var else string
+        //React functional component can only return only one ele
+        <div>
+            <div>{greeting}</div>
+            <div>{followup_greeting}</div>
+            <div> Computing {num1} + {num2} to {num1 + num2}</div>
+            //internal link react router dom
+            <a href={link}> Go to google</a>
+        </div>
     )
 }
 
