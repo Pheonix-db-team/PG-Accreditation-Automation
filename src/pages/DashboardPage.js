2e2e898d0b8f612528cbfb1e6249830520e3a007
Yugam <yugam11parashar@gmail.com>
2023-03-07 13:40:38 +0530

    Read data working:rocket:

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 function DashboardPage() {
     let { state } = useLocation();
-    console.log(state);
+    console.log(state.user_email);
+    const [studentList, setStudentList] = useState([])
+    const studentsCollectionRef = collection(db, "test_pilot");
+    // key is collection name
+    useEffect(() => {
+        const getStudentList = async () => {
+
+            //Read data
+
+            const getStudentList = async () => {
+
+            }
+            // const  querySnapshot = await getDocs(
+            //     studentsCollectionRef
+            // );
+            // //better readabiloty
+            // querySnapshot.forEach((doc) => {
+            //     console.log(`${doc.id} => ${doc.data()}`);
+            //   });
+            //set state to that data	
+            //const data= await getDocs()
+
+            try {
+                const data = await getDocs(studentsCollectionRef);
+                console.log(data);
+            }
+            catch (err) {
+                console.error(err);
+            }
+
+        }; getStudentList();
+    }
+        , []);
+
     return (
         <div>DashboardPage
             <br></br>
