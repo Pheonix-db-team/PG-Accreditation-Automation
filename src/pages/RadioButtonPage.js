2059341c246c3047ec01de88ae08f6e04481251a
Yugam <yugam11parashar@gmail.com>
2023-03-10 21:32:07 +0530

    :sparkles:Radio button implementation from Map

--- src/pages/RadioButtonPage.js
+++ src/pages/RadioButtonPage.js
@@ -4,6 +4,15 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 function RadioButtonPage() {
     const arr = ['option1', 'option2', 'option3'];
+    const ques = {
+        question_prompt: "How was the course",
+        option_A: "Poor",
+        option_B: "Satisfactory",
+        option_C: "Good",
+        option_D: "Very Good",
+        tag: 0
+    }
+
     const [option, setOption] = useState(arr[0]);
     const handleRadioChange = (e) => {
         console.log("Set option called");
@@ -11,26 +20,12 @@ function RadioButtonPage() {
     }
     return (
         <body>
+            {ques['question_prompt']}<br></br>
+            <label ><input type="radio" name={ques['tag']} value={ques['option_A']} defaultChecked={option === ques['option_A']} onChange={handleRadioChange} />{ques['option_A']}</label>
+            <label ><input type="radio" name={ques['tag']} value={ques['option_B']} defaultChecked={option === ques['option_B']} onChange={handleRadioChange} />{ques['option_B']}</label>
+            <label ><input type="radio" name={ques['tag']} value={ques['option_C']} defaultChecked={option === ques['option_C']} onChange={handleRadioChange} />{ques['option_C']}</label>
+            <label ><input type="radio" name={ques['tag']} value={ques['option_D']} defaultChecked={option === ques['option_D']} onChange={handleRadioChange} />{ques['option_D']}</label>
 
-            <label >
-                <input type="radio" name="myRadio" value="option1" defaultChecked={option === "option1"} onChange={handleRadioChange} />
-                Option 1
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    name="myRadio"
-                    value="option2"
-                    defaultChecked={option === "option2"}
-                    onChange={handleRadioChange}
-                />
-                Option 2
-            </label>
-            <label>
-                <input type="radio" name="myRadio" value="option3" defaultChecked={option === "option3"}
-                    onChange={handleRadioChange} />
-                Option 3
-            </label>
             <br></br>
             option is {option}
 
