9e646f17b0fd62b65cbef98dc440a6d565f1d425
SUBE007 <chaurasiyasube007@gmail.com>
2023-03-21 02:11:15 +0530

    seprated all file categorey wise did some modification

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,51 +9,55 @@ function AboutPage() {
         <div>
                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}><h1>About Page</h1></div>
                 <h2>Course Exit Survey</h2>
-                <span><p1>In order for the PG Accreditation Automation system to function smoothly
-without any performance issues, threshold criteria for the number of residents
-simultaneously accessing the platform and uploading related attachments must
-be properly defined according to the server side system capabilities. Also the
-connection to the system is based on attributes of the user like his/her location,
-and the system will be working 24x7, so quality of network connectivity at the
-user end will also affect the system performance.</p1></span>
+                <span>
+                  <p1>
+                      In order for the PG Accreditation Automation system to function smoothly
+                      without any performance issues, threshold criteria for the number of residents
+                      simultaneously accessing the platform and uploading related attachments must
+                      be properly defined according to the server side system capabilities. Also the
+                      connection to the system is based on attributes of the user like his/her location,
+                      and the system will be working 24x7, so quality of network connectivity at the
+                      user end will also affect the system performance.
+                  </p1>
+                </span>
 
 
 <h2>Team</h2>
 <View style={styles.Container}>
 
         <Image
-          source={require('./yugam.jpg')}
+          source={require('../image/yugam.jpg')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Lead Developer: YUGAM PARASHAR</Text>
 
 
         <Image
-          source={require('./subedar.jpg')}
+          source={require('../image/subedar.png')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Developer: SUBEDAR CHAURASIYA</Text>
 
         <Image
-          source={require('./sujeet.jpg')}
+          source={require('../image/sujeet.jpg')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Developer: SUJEET KUMAR SINGH</Text>
 
         <Image
-          source={require('./sushal.jpg')}
+          source={require('../image/sushal.jpg')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Developer: SUSHAL DEVASARI</Text>
 
         <Image
-          source={require('./Swapnil.jpg')}
+          source={require('../image/Swapnil.jpg')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Project Coordinator: SWAPNIL BAVISKAR</Text>
 
         <Image
-          source={require('./vaibhav.jpg')}
+          source={require('../image/vaibhav.jpg')}
           style={styles.circleImageLayout}
         />
         <Text style={styles.text}>Developer & Query Handler: VAIBHAV RAIBOLE</Text>
